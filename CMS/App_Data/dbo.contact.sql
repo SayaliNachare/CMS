23c7a056397836db123133d454f36577c6633516
SayaliNachare <137667203+SayaliNachare@users.noreply.github.com>
2023-06-30 09:34:31 +0530

    Add files via upload

---
+++ CMS/App_Data/dbo.contact.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[contact] (
+    [Name]    VARCHAR (100)  NULL,
+    [EmailID] VARCHAR (100)  NULL,
+    [Phoneno] VARCHAR (100)  NULL,
+    [Subject] VARCHAR (100)  NULL,
+    [Message] VARCHAR (1000) NULL
+);
+
